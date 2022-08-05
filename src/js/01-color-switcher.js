cb5cb3a6dda6fe3c976c1e2ad61f5db1279ce15a
Kate Vaskevich <kate.panasiuk@yahoo.com>
2022-08-05 17:37:06 +0300

    First Task Solved and update to the json file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+refs.buttonStart.addEventListener('click', colorBackgroundClick);
+refs.buttonStop.addEventListener('click', stopColorBackgroundClick);
+
+let isActive = false;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorBackgroundClick() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(() => {
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorBackgroundClick() {
+  clearInterval(intervalId);
+  isActive = false;
+}
