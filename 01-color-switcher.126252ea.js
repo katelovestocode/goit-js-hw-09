6aefcca461ff06fe71b7ad606a734ff0e3ce9397
katelovestocode <97043532+katelovestocode@users.noreply.github.com>
2022-08-11 10:21:00 +0000

    Deploying to gh-pages from @ katelovestocode/goit-js-hw-09@47f5b34491f3610f26736ed8b22fa5d26d98d914 🚀

--- 01-color-switcher.126252ea.js
+++
@@ -1,2 +0,0 @@
-const t={buttonStart:document.querySelector("button[data-start]"),buttonStop:document.querySelector("button[data-stop]"),bodyEl:document.querySelector("body")};t.buttonStart.addEventListener("click",(function(){if(o)return;o=!0,e=setInterval((()=>{t.bodyEl.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.buttonStop.addEventListener("click",(function(){clearInterval(e),o=!1}));let o=!1,e=null;
-//# sourceMappingURL=01-color-switcher.126252ea.js.map
